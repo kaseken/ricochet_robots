89b5a2f95b57922fa57b129120abe408fd9e5232
kaseken <k.kaseda0925@gmail.com>
2022-05-01 16:01:13 +0900

    feat: update GameState

--- lib/domains/game/game_state.dart
+++ lib/domains/game/game_state.dart
@@ -1,7 +1,9 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:ricochet_robots/models/board.dart';
+import 'package:ricochet_robots/models/board_builder.dart';
 import 'package:ricochet_robots/models/goal.dart';
 import 'package:ricochet_robots/models/history.dart';
+import 'package:ricochet_robots/models/position.dart';
 import 'package:ricochet_robots/models/robot.dart';
 
 part 'game_state.freezed.dart';
@@ -23,8 +25,86 @@ class GameState with _$GameState {
 
     /// Properties for editing.
     required Board customBoard,
-    required Robot selectedRobotForEdit,
+    required Robot? selectedRobotForEdit,
   }) = _GameState;
 
   bool get isEditMode => mode == GameWidgetMode.editBoard;
+
+  /// Reset state and returns new state.
+  static GameState reset({Board? newBoard}) {
+    final board = newBoard ?? Board(grids: BoardBuilder.buildDefaultGrids());
+    return GameState(
+      mode: GameWidgetMode.play,
+      board: board,
+      goal: GoalBuilder.build(),
+      histories: List.empty(growable: true),
+      focusedRobot: const Robot(color: RobotColors.red),
+      customBoard: board,
+      selectedRobotForEdit: null,
+    );
+  }
+
+  /// Switches from the given mode, and returns new state.
+  GameState switchMode({required GameWidgetMode from}) {
+    switch (from) {
+      case GameWidgetMode.play:
+        return copyWith(customBoard: board, mode: GameWidgetMode.editBoard);
+      case GameWidgetMode.editBoard:
+        return reset(newBoard: customBoard);
+    }
+  }
+
+  GameState onTapGrid({required int x, required int y}) {
+    final tappedPosition = Position(x: x, y: y);
+    final maybeExistingRobot = customBoard.getRobotIfExists(tappedPosition);
+    if (maybeExistingRobot != null) {
+      /// Select robot on tapped grid.
+      return copyWith(selectedRobotForEdit: maybeExistingRobot);
+    }
+
+    final selectedRobot = selectedRobotForEdit;
+    if (selectedRobot == null || customBoard.hasGoalOnGrid(tappedPosition)) {
+      return this;
+    }
+    return copyWith(
+      customBoard: customBoard.movedTo(selectedRobot, tappedPosition),
+      selectedRobotForEdit: null,
+    );
+  }
+
+  GameState onDirectionSelected(Directions direction) {
+    final currentPosition = board.robotPositions[focusedRobot.color];
+    if (currentPosition == null) {
+      return this;
+    }
+    final nextBoard = board.moved(focusedRobot, direction);
+    final nextPosition = board.robotPositions[focusedRobot.color];
+    if (nextPosition == null) {
+      return copyWith(board: nextBoard);
+    }
+    final history = History(
+      color: focusedRobot.color,
+      position: currentPosition,
+    );
+    final nextHistories = !nextPosition.equals(currentPosition)
+        ? (histories..add(history))
+        : histories;
+    if (board.isGoal(nextPosition, goal, focusedRobot)) {
+      return copyWith(board: nextBoard, histories: nextHistories);
+    }
+    return reset();
+  }
+
+  GameState onRedoPressed() {
+    if (histories.isEmpty) {
+      return this;
+    }
+    final prevHistory = histories.removeLast();
+    return copyWith(
+      board: board.movedTo(
+        Robot(color: prevHistory.color),
+        prevHistory.position,
+      ),
+    );
+  }
 }
