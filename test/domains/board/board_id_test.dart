5534d54aae08d5e0bc4352350fad4cd63014ad5c
kaseken <k.kaseda0925@gmail.com>
2022-05-04 01:19:25 +0900

    test: fix test

--- test/domains/board/board_id_test.dart
+++ test/domains/board/board_id_test.dart
@@ -11,15 +11,15 @@ void main() {
       final grids = BoardBuilder.defaultGrids;
       final robotPositions = Map.fromEntries([
         const MapEntry(RobotColors.red, Position(x: 12, y: 12)),
-        const MapEntry(RobotColors.blue, Position(x: 1, y: 9)),
-        const MapEntry(RobotColors.green, Position(x: 7, y: 15)),
+        const MapEntry(RobotColors.blue, Position(x: 14, y: 2)),
+        const MapEntry(RobotColors.green, Position(x: 15, y: 7)),
         const MapEntry(RobotColors.yellow, Position(x: 0, y: 13)),
       ]);
       final board = Board(grids: grids, robotPositions: robotPositions);
       final boardId = BoardId.from(board: board);
       expect(
         boardId.value,
-        '6c6eaeeeeeec6aec7bfd6fffffffd6fd7c7ffffff97ffffd7fffff97fefffff97fffffeffffffffc3ffffffffffbfd3d6fd3fffbbffc7fed7ffefbd007fffffd7ffffc5007fffffd797ffffeafffff957efd3fffc7ffffe93fffefffffffd3fc6ffffbffffbffefd7ffbfc7ffd6ffffd7fd6fffffffffffd3bbbbb93bbb93bb95dade563d1364ae98a92123e4119dbb658cc197f0d',
+        '6c6eaeeeeeec6aec7bfd6fffffffd6fd7c7ffffff97ffffd7fffff97fefffff97fffffeffffffffc3ffffffffffbfd3d6fd3fffbbffc7fed7ffefbd007fffffd7ffffc5007fffffd797ffffeafffff957efd3fffc7ffffe93fffefffffffd3fc6ffffbffffbffefd7ffbfc7ffd6ffffd7fd6fffffffffffd3bbbbb93bbb93bb95dade563d1364ae98a92123e4119dbb658cce2f70d',
       );
     });
   });
