d9f7f5a8d4170cff3134881a532418774f6bb334
kaseken <k.kaseda0925@gmail.com>
2022-05-15 22:41:48 +0900

    fix: add gamma board

--- lib/domains/board/defaultBoards/green/boards.dart
+++ lib/domains/board/defaultBoards/green/boards.dart
@@ -6,9 +6,10 @@ import 'package:ricochet_robots/domains/board/position.dart';
 import 'package:ricochet_robots/domains/board/robot.dart';
 
 BoardQuarterGreen randomGreenBoard() => [
-      // _defaultGreenAlpha,
+      _defaultGreenAlpha,
       _defaultGreenBeta,
-    ][Random().nextInt(1)];
+      _defaultGreenGamma,
+    ][Random().nextInt(3)];
 
 final _defaultGreenAlpha = BoardQuarterGreen.from(
   goals: {
@@ -83,6 +84,45 @@ final _defaultGreenBeta = BoardQuarterGreen.from(
     const WallPosition(x: 6, y: 5),
     const WallPosition(x: 0, y: 6),
     const WallPosition(x: 3, y: 7),
+    const WallPosition(x: 7, y: 7),
+    const WallPosition(x: 7, y: 8),
+  },
+);
+
+final _defaultGreenGamma = BoardQuarterGreen.from(
+  goals: {
+    const Position(x: 1, y: 4): const Goal(
+      color: RobotColors.red,
+      type: GoalTypes.moon,
+    ),
+    const Position(x: 3, y: 1): const Goal(
+      color: RobotColors.green,
+      type: GoalTypes.sun,
+    ),
+    const Position(x: 6, y: 3): const Goal(
+      color: RobotColors.yellow,
+      type: GoalTypes.star,
+    ),
+    const Position(x: 4, y: 6): const Goal(
+      color: RobotColors.blue,
+      type: GoalTypes.planet,
+    ),
+  },
+  verticalWalls: {
+    const WallPosition(x: 2, y: 0),
+    const WallPosition(x: 3, y: 1),
+    const WallPosition(x: 7, y: 3),
+    const WallPosition(x: 1, y: 4),
+    const WallPosition(x: 5, y: 6),
+    const WallPosition(x: 7, y: 7),
+    const WallPosition(x: 8, y: 7),
+  },
+  horizontalWalls: {
+    const WallPosition(x: 3, y: 1),
+    const WallPosition(x: 6, y: 4),
+    const WallPosition(x: 1, y: 5),
+    const WallPosition(x: 4, y: 6),
+    const WallPosition(x: 0, y: 7),
     const WallPosition(x: 7, y: 7),
     const WallPosition(x: 7, y: 8),
   },
--- lib/domains/board/defaultBoards/red/boards.dart
+++ lib/domains/board/defaultBoards/red/boards.dart
@@ -6,9 +6,10 @@ import 'package:ricochet_robots/domains/board/position.dart';
 import 'package:ricochet_robots/domains/board/robot.dart';
 
 BoardQuarterRed randomRedBoard() => [
-      // _defaultRedAlpha,
+      _defaultRedAlpha,
       _defaultRedBeta,
-    ][Random().nextInt(1)];
+      _defaultRedGamma,
+    ][Random().nextInt(3)];
 
 final _defaultRedAlpha = BoardQuarterRed.from(
   goals: {
@@ -83,6 +84,45 @@ final _defaultRedBeta = BoardQuarterRed.from(
     const WallPosition(x: 2, y: 5),
     const WallPosition(x: 7, y: 5),
     const WallPosition(x: 0, y: 6),
+    const WallPosition(x: 7, y: 7),
+    const WallPosition(x: 7, y: 8),
+  },
+);
+
+final _defaultRedGamma = BoardQuarterRed.from(
+  goals: {
+    const Position(x: 2, y: 4): const Goal(
+      color: RobotColors.green,
+      type: GoalTypes.moon,
+    ),
+    const Position(x: 7, y: 5): const Goal(
+      color: RobotColors.red,
+      type: GoalTypes.sun,
+    ),
+    const Position(x: 1, y: 6): const Goal(
+      color: RobotColors.yellow,
+      type: GoalTypes.planet,
+    ),
+    const Position(x: 5, y: 2): const Goal(
+      color: RobotColors.blue,
+      type: GoalTypes.star,
+    ),
+  },
+  verticalWalls: {
+    const WallPosition(x: 4, y: 0),
+    const WallPosition(x: 6, y: 2),
+    const WallPosition(x: 3, y: 4),
+    const WallPosition(x: 7, y: 5),
+    const WallPosition(x: 1, y: 6),
+    const WallPosition(x: 7, y: 7),
+    const WallPosition(x: 8, y: 7),
+  },
+  horizontalWalls: {
+    const WallPosition(x: 5, y: 3),
+    const WallPosition(x: 2, y: 4),
+    const WallPosition(x: 0, y: 5),
+    const WallPosition(x: 1, y: 6),
+    const WallPosition(x: 7, y: 6),
     const WallPosition(x: 7, y: 7),
     const WallPosition(x: 7, y: 8),
   },
