fbe27c0f5778a0e344c6947ae78bf8dd866dd249
kaseken <kaseken@users.noreply.github.com>
2022-06-05 13:32:05 +0000

    deploy: 3cb27ad32c8a12721582083905f6d053a698e718

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,25 +3,25 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "30f11c7b8ed99c1c68779b135c9a9214",
+  "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "ba4d2e1497046fa6af3c14ed8fe85b07",
+"/": "ba4d2e1497046fa6af3c14ed8fe85b07",
+"main.dart.js": "30f11c7b8ed99c1c68779b135c9a9214",
+"version.json": "89f7a7788fa85a560e48ab312a33dc07",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
-"flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
-"index.html": "07875926aff7f6bed6560d3e084eeed5",
-"/": "07875926aff7f6bed6560d3e084eeed5",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"version.json": "89f7a7788fa85a560e48ab312a33dc07",
-"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"assets/NOTICES": "51cdc13c9c4daeed7fa5bf3e6125940a",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"manifest.json": "b19cc0a9bc6ab8489673015e7d81b873",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1"
+"assets/NOTICES": "51cdc13c9c4daeed7fa5bf3e6125940a",
+"manifest.json": "2aae1936ff9525bed43a842858076695"
 };
 
 // The application shell files that are downloaded before a service worker can
